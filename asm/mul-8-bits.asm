4d0335732ed6bb7e1425b7bb5cb075f4ad6f697f
namber0 <namnb2402@gmail.com>
2024-04-24 17:50:50 +0700

    Add add16 assembly program

--- asm/mul-8-bits.asm
+++ asm/mul-8-bits.asm
@@ -6,22 +6,23 @@ LDA 5000     ;1st value, Load A from memory location 5000
 MOV B, A     ;copy content of A to B
 DCR B        ;Decrement B
 ORA A        ;Check is value in A (or B) is 0 or not
-JZ Exit      ;if Zero, jump to exit
+JNZ END      ;if Zero, jump to exit
 
 LDA 5001     ;2nd value, Load A from memory location 5001
 MOV D, A     ;copy content of A to D
 ORA A        ;Check if value of A (or D) is 0 or not
 
-Loop: 
-JZ Exit      ;if Zero, jump to exit
+LOOP: 
+JZ END      ;if Zero, jump to exit
 ADD D        ;Add D to A
-JNC NoCarry  ;if carry is not generated, jump to label NoCarry
+JNC NOCARRY  ;if carry is not generated, jump to label NOCARRY
 INR C        ;if carry is generated, increment C
-NoCarry: 
-DCR B        ;decrement B
-JMP Loop     ;Jump to label Loop
 
-Exit: 
+NOCARRY: 
+DCR B        ;decrement B
+JMP LOOP     ;Jump to label LOOP
+
+END: 
 STA 5003     ;store content of A (lower order of result) at memory location 5003
 MOV A, C     ;copy content of C to A
 STA 5002     ;store content of A(higher order of resut) at memory location 5002
