afd31596759ff8bb8349e3fca75cd58c4d1124ca
namber0 <namnb2402@gmail.com>
2024-04-24 17:55:00 +0700

    Update the mul and add assembly test files

--- asm/add16.asm
+++ asm/add16.asm
@@ -5,23 +5,23 @@
 
 
 ;Load 1st Number in HL register pair
-LDA 5000   ;Load low-order
+LDA 5000H  ;Load low-order
 MOV L, A   ;copy low-oder in L
-LDA 5001   ;Load high-order
+LDA 5001H  ;Load high-order
 MOV H, A   ;copy high-oder in H
 
 ;Load 2nd Number in DE register pair
-LDA 5002   ;Load low-order
+LDA 5002H   ;Load low-order
 MOV E, A   ;copy low-oder in E
-LDA 5003   ;Load high-order
+LDA 5003H   ;Load high-order
 MOV D, A   ;copy high-oder in D
 
 DAD D      ;HL = HL + DE (Add 16 bit value)
 
 ;Store result
 MOV A, L   ;copy low-oder in A
-STA 5004   ;Store low-order
+STA 5004H  ;Store low-order
 MOV A, H   ;copy high-oder in A
-STA 5005   ;Store high-order
+STA 5005H  ;Store high-order
 
 HLT        ;Halt Program
--- asm/mul-8-bits.asm
+++ asm/mul-8-bits.asm
@@ -2,13 +2,13 @@
 
 MVI C, 00    ;Store 00 in C 
 
-LDA 5000     ;1st value, Load A from memory location 5000
+LDA 5000H    ;1st value, Load A from memory location 5000
 MOV B, A     ;copy content of A to B
 DCR B        ;Decrement B
 ORA A        ;Check is value in A (or B) is 0 or not
 JNZ END      ;if Zero, jump to exit
 
-LDA 5001     ;2nd value, Load A from memory location 5001
+LDA 5001H    ;2nd value, Load A from memory location 5001
 MOV D, A     ;copy content of A to D
 ORA A        ;Check if value of A (or D) is 0 or not
 
@@ -23,7 +23,7 @@ DCR B        ;decrement B
 JMP LOOP     ;Jump to label LOOP
 
 END: 
-STA 5003     ;store content of A (lower order of result) at memory location 5003
+STA 5003H    ;store content of A (lower order of result) at memory location 5003
 MOV A, C     ;copy content of C to A
-STA 5002     ;store content of A(higher order of resut) at memory location 5002
+STA 5002H    ;store content of A(higher order of resut) at memory location 5002
 HLT          ;Halt program
